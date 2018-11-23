ca7f7a7fa5d326d1dd864c20fc40bdbe52a1f5e2
mustafa1mashtah <mashtah.moustafa@gmail.com>
2018-11-23 11:08:49 +0100

    evaluation2

--- src/evaluationjava2/NumberMirror.java
+++ src/evaluationjava2/NumberMirror.java
@@ -1,4 +1,52 @@
 package evaluationjava2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class NumberMirror {
+    public Integer mirror(Integer number) {
+
+
+        String newNumber = number.toString();
+        String[] split = newNumber.split("");
+        List<String> numbers = new ArrayList<>(Arrays.asList(split));
+        Collections.reverse(numbers);
+        String reversed = "";
+        for (String string : numbers) {
+            reversed += string;
+        }
+        Integer reversedNumber = Integer.valueOf(reversed);
+        return reversedNumber;
+
+
+    }
+    public Integer specialMirror(Integer number){
+        String newNumber = number.toString();
+        String[] split = newNumber.split("");
+        List<String> numbers = new ArrayList<>(Arrays.asList(split));
+        String modifedNumbers="";
+        for (String string:numbers
+             ) {
+            Integer modifidNumber = Integer.valueOf(string);
+            if (modifidNumber==9){
+                modifidNumber=0;
+            }
+            else {
+                modifidNumber += 1;
+            }
+                String newNumber1 = modifidNumber.toString();
+                modifedNumbers = modifedNumbers + newNumber1;
+
+
+        }
+        Integer modifedNumber = Integer.valueOf(modifedNumbers);
+
+        return modifedNumber;
+
+
+    }
+
+
 }
--- src/evaluationjava2/NumberMirrorApplication.java
+++ src/evaluationjava2/NumberMirrorApplication.java
@@ -1,4 +1,20 @@
 package evaluationjava2;
 
+import java.util.Scanner;
+
 public class NumberMirrorApplication {
+    public static void main(String[] args) {
+        System.out.println("please enter a number");
+        Scanner scanner=new Scanner(System.in);
+        Integer number=scanner.nextInt();
+
+        NumberMirror numberMirror=new NumberMirror();
+        Integer reversedNumber=numberMirror.mirror(number);
+        Integer modifeidNumber=numberMirror.specialMirror(reversedNumber);
+        System.out.println("the number is "+ number);
+        System.out.println("the reverced number is "+ reversedNumber);
+        System.out.println("the modifeid number is "+ modifeidNumber);
+
+
+    }
 }
